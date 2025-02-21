0539b9fb9ca27371ae0118cd92fa798c895b5fe7
teyper <teyjhanabartonneal@spelman.edu>
2025-02-20 22:00:18 -0500

    final touches

---
+++ Assets/ASSETS.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ASSETS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ASSET ATTRIBUTIONS
+        //Kamala Harris: https://sketchfab.com/search?features=downloadable&q=computer+wire&type=models 
+        //Wooden flloor texture:https://www.texturebox.com/category 
+        //Third person follow camera: https://assetstore.unity.com/?utm_source=google&utm_medium=cpc&utm_campaign=as_as_as_amer_ca_en_pu_sem-gg_acq_pr_2024-04_brand_cc3022_ev-br_id:71700000118127131&utm_content=as_as_as_amer_pu_sem_gg_ev-br_pros_x_npd_cpc_kw_sd_all_sts_x_3d-assets_id:58700008694283264&utm_term=unity%20store&gad_source=1&gclid=Cj0KCQiAwtu9BhC8ARIsAI9JHakAL4Ad2JQjlv-mUdoYwaxmELuSQNQOZuKH1WkjYC17bFoCX6zB4GQaAl4kEALw_wcB&gclsrc=aw.ds 
+        // armature and animation: https://www.mixamo.com/#/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
